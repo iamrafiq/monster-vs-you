b9ad250f6732c62a48f38fe4a46088181cbdbd44
linuxrafiq <rafiq.i@live.co.uk>
2020-06-06 18:38:54 +0600

    attack is working

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 new Vue({
-    el:'#app',
-    data: function() {
-        return {
-            playerHealth: 100,
-            monsterHealth: 100,
-            gameIsRunning: false
-        }
+  el: "#app",
+  data: function () {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      gameIsRunning: false,
+    };
+  },
+  methods: {
+    startGame: function () {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
     },
+    attack: function () {
+      var max = 10;
+      var min = 3;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.monsterHealth -= damage;
+
+      if(this.monsterHealth <= 0){
+          alert('You Won!');
+          this.gameIsRunning = false;
+          return;
+      }
+      max = 12;
+      min = 5;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth -= damage;
+
+      if (this.playerHealth <= 0){
+          alert('You Loose');
+          this.gameIsRunning = false;
+          return;
+      }
+
+
+    },
+    specialAttack: function () {},
+    heal: function () {},
+    giveUp: function () {},
+  },
 });
